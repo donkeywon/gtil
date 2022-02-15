ab21596834284454b245dfad90edfc7e004ee693
donkeywon <donkeywon@163.com>
2022-02-15 11:03:01 +0800

    base service

--- service/service.go
+++ service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"context"
+	"go.uber.org/multierr"
 	"go.uber.org/zap"
 )
 
@@ -11,5 +13,29 @@ type Service interface {
 	Shutdown() error
 	WithLogger(logger *zap.Logger)
 	Statistics() map[string]float64
+	AppendError(err error)
 	LastError() error
+}
+
+type BaseService struct {
+	logger *zap.Logger
+	ctx    context.Context
+	cancel context.CancelFunc
+	err    error
+}
+
+func (bs *BaseService) AppendError(err error) {
+	bs.err = multierr.Append(bs.err, err)
+}
+
+func (bs *BaseService) LastError() error {
+	return bs.err
+}
+
+func (bs *BaseService) WithLogger(logger *zap.Logger) {
+	bs.logger = logger.Named(bs.Name())
+}
+
+func (bs *BaseService) Name() string {
+	panic("Override me")
 }
