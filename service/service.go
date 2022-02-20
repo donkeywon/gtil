eda836af0ec2d9166aff1d4fc31f430d5816c59a
donkeywon <donkeywon@163.com>
2022-02-20 09:14:01 +0800

    顺序启动children

--- service/service.go
+++ service/service.go
@@ -1,208 +1,211 @@
 package service
 
 import (
-    "context"
-    "github.com/pkg/errors"
-    "go.uber.org/multierr"
-    "go.uber.org/zap"
+	"context"
+	"github.com/pkg/errors"
+	"go.uber.org/multierr"
+	"go.uber.org/zap"
 )
 
 type Service interface {
-    Name() string
-    Open() error
-    Close() error
-    Shutdown() error
+	Name() string
+	Open() error
+	Close() error
+	Shutdown() error
 
-    WithLogger(self Service, logger *zap.Logger)
-    WithContext(ctx context.Context)
-    Context() context.Context
+	WithLogger(self Service, logger *zap.Logger)
+	WithContext(ctx context.Context)
+	Context() context.Context
 
-    Closed() <-chan struct{}
-    CloseCh()
-    ListenAndClose(self Service)
+	Closed() <-chan struct{}
+	CloseCh()
+	ListenAndClose(self Service)
 
-    Children() map[string]Service
-    OpenChildren() error
-    CloseChildren()
-    WaitChildrenClose()
-    ChildrenLastError() error
-    WithChildContext(ctx context.Context, cancel context.CancelFunc)
-    AppendService(name string, svc Service)
+	Children() map[string]Service
+	OpenChildren() error
+	CloseChildren()
+	WaitChildrenClose()
+	ChildrenLastError() error
+	WithChildContext(ctx context.Context, cancel context.CancelFunc)
+	AppendService(name string, svc Service)
 
-    Statistics() map[string]float64
-    AppendError(err ...error)
-    LastError() error
+	Statistics() map[string]float64
+	AppendError(err ...error)
+	LastError() error
 
-    Debug(msg string, fields ...zap.Field)
-    Info(msg string, fields ...zap.Field)
-    Warn(msg string, fields ...zap.Field)
-    Error(msg string, fields ...zap.Field)
-    Panic(msg string, fields ...zap.Field)
-    DPanic(msg string, fields ...zap.Field)
-    Fatal(msg string, fields ...zap.Field)
+	Debug(msg string, fields ...zap.Field)
+	Info(msg string, fields ...zap.Field)
+	Warn(msg string, fields ...zap.Field)
+	Error(msg string, fields ...zap.Field)
+	Panic(msg string, fields ...zap.Field)
+	DPanic(msg string, fields ...zap.Field)
+	Fatal(msg string, fields ...zap.Field)
 }
 
 func DoOpen(self Service, ctx context.Context, logger *zap.Logger) error {
-    self.WithLogger(self, logger)
-    self.WithContext(ctx)
-    self.WithChildContext(context.WithCancel(context.Background()))
+	self.WithLogger(self, logger)
+	self.WithContext(ctx)
+	self.WithChildContext(context.WithCancel(context.Background()))
 
-    self.Info("Opening")
-    defer self.Info("Opened")
+	self.Info("Opening")
+	defer self.Info("Opened")
 
-    err := self.OpenChildren()
-    if err != nil {
-        return errors.Wrapf(err, ErrOpenSvc, self.Name())
-    }
+	err := self.OpenChildren()
+	if err != nil {
+		return errors.Wrapf(err, ErrOpenSvc, self.Name())
+	}
 
-    err = self.Open()
-    if err != nil {
-        return errors.Wrapf(err, ErrOpenSvc, self.Name())
-    }
+	err = self.Open()
+	if err != nil {
+		return errors.Wrapf(err, ErrOpenSvc, self.Name())
+	}
 
-    go self.ListenAndClose(self)
-    return nil
+	go self.ListenAndClose(self)
+	return nil
 }
 
 func DoClose(self Service) error {
-    self.Info("Closing")
-    defer self.Info("Closed")
+	self.Info("Closing")
+	defer self.Info("Closed")
 
-    select {
-    case <-self.Closed():
-        return nil
-    default:
-        defer self.CloseCh()
+	select {
+	case <-self.Closed():
+		return nil
+	default:
+		defer self.CloseCh()
 
-        self.CloseChildren()
-        self.WaitChildrenClose()
+		self.CloseChildren()
+		self.WaitChildrenClose()
 
-        err := multierr.Combine(self.ChildrenLastError(), self.Close())
-        if err != nil {
-            return errors.Wrapf(err, ErrCloseSvc, self.Name())
-        }
+		err := multierr.Combine(self.ChildrenLastError(), self.Close())
+		if err != nil {
+			return errors.Wrapf(err, ErrCloseSvc, self.Name())
+		}
 
-        return nil
-    }
+		return nil
+	}
 }
 
 func DoShutdown(self Service) error {
-    self.Info("Shutting down")
-    defer self.Info("Shutdown")
-    select {
-    case <-self.Closed():
-        return nil
-    default:
-        defer self.CloseCh()
+	self.Info("Shutting down")
+	defer self.Info("Shutdown")
+	select {
+	case <-self.Closed():
+		return nil
+	default:
+		defer self.CloseCh()
 
-        var err error
-        for _, child := range self.Children() {
-            err = multierr.Append(err, DoShutdown(child))
-        }
-        err = multierr.Append(err, self.Shutdown())
+		var err error
+		for _, child := range self.Children() {
+			err = multierr.Append(err, DoShutdown(child))
+		}
+		err = multierr.Append(err, self.Shutdown())
 
-        if err != nil {
-            return errors.Wrapf(err, ErrShutdownSvc, self.Name())
-        }
+		if err != nil {
+			return errors.Wrapf(err, ErrShutdownSvc, self.Name())
+		}
 
-        return nil
-    }
+		return nil
+	}
 }
 
 type BaseService struct {
-    *zap.Logger
-    ctx         context.Context
-    err         error
-    closed      chan struct{}
-    children    map[string]Service
-    childCtx    context.Context
-    childCancel context.CancelFunc
+	*zap.Logger
+	ctx         context.Context
+	err         error
+	closed      chan struct{}
+	children    map[string]Service
+	childrenArr []Service
+	childCtx    context.Context
+	childCancel context.CancelFunc
 }
 
 func NewBase() *BaseService {
-    return &BaseService{
-        closed:   make(chan struct{}),
-        children: make(map[string]Service),
-    }
+	return &BaseService{
+		closed:      make(chan struct{}),
+		children:    make(map[string]Service),
+		childrenArr: make([]Service, 0, 1),
+	}
 }
 
 func (bs *BaseService) WithLogger(self Service, logger *zap.Logger) {
-    bs.Logger = logger.Named(self.Name())
+	bs.Logger = logger.Named(self.Name())
 }
 
 func (bs *BaseService) WithContext(ctx context.Context) {
-    bs.ctx = ctx
+	bs.ctx = ctx
 }
 
 func (bs *BaseService) Context() context.Context {
-    return bs.ctx
+	return bs.ctx
 }
 
 func (bs *BaseService) Closed() <-chan struct{} {
-    return bs.closed
+	return bs.closed
 }
 
 func (bs *BaseService) CloseCh() {
-    close(bs.closed)
+	close(bs.closed)
 }
 
 func (bs *BaseService) ListenAndClose(self Service) {
-    <-bs.ctx.Done()
-    bs.Debug("Receive cancel, start close")
-    bs.AppendError(DoClose(self))
+	<-bs.ctx.Done()
+	bs.Debug("Receive cancel, start close")
+	bs.AppendError(DoClose(self))
 }
 
-func (bs *BaseService) Children() map[string]Service {
-    return bs.children
+func (bs *BaseService) Children(name string) Service {
+	return bs.children[name]
 }
 
 func (bs *BaseService) OpenChildren() error {
-    var err error
-    for _, child := range bs.children {
-        err = multierr.Append(err, DoOpen(child, bs.childCtx, bs.Logger))
-    }
-    return err
+	var err error
+	for _, child := range bs.childrenArr {
+		err = multierr.Append(err, DoOpen(child, bs.childCtx, bs.Logger))
+	}
+	return err
 }
 
 func (bs *BaseService) CloseChildren() {
-    bs.childCancel()
+	bs.childCancel()
 }
 
 func (bs *BaseService) WaitChildrenClose() {
-    for _, child := range bs.children {
-        <-child.Closed()
-    }
+	for _, child := range bs.childrenArr {
+		<-child.Closed()
+	}
 }
 
 func (bs *BaseService) ChildrenLastError() error {
-    var err error
-    for _, child := range bs.children {
-        err = multierr.Append(err, child.LastError())
-    }
-    return err
+	var err error
+	for _, child := range bs.childrenArr {
+		err = multierr.Append(err, child.LastError())
+	}
+	return err
 }
 
 func (bs *BaseService) WithChildContext(ctx context.Context, cancel context.CancelFunc) {
-    bs.childCtx = ctx
-    bs.childCancel = cancel
+	bs.childCtx = ctx
+	bs.childCancel = cancel
 }
 
 func (bs *BaseService) AppendService(name string, svc Service) {
-    if name == "" || svc == nil {
-        return
-    }
+	if name == "" || svc == nil {
+		return
+	}
 
-    bs.children[name] = svc
+	bs.children[name] = svc
+	bs.childrenArr = append(bs.childrenArr, svc)
 }
 
 func (bs *BaseService) AppendError(err ...error) {
-    bs.err = multierr.Append(bs.err, multierr.Combine(err...))
+	bs.err = multierr.Append(bs.err, multierr.Combine(err...))
 }
 
 func (bs *BaseService) Statistics() map[string]float64 {
-    return nil
+	return nil
 }
 
 func (bs *BaseService) LastError() error {
-    return bs.err
+	return bs.err
 }
